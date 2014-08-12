a6c5dbaebacac9fca98921e01f5b2109db926447
Ivo Georgiev <ivo@linvo.org>
2014-08-12 13:56:43 +0300

    Transparency misses

--- src/browser/native_window_mac.h
+++ src/browser/native_window_mac.h
@@ -80,6 +80,8 @@ class NativeWindowCocoa : public NativeWindow {
   virtual void SetToolbarIsLoading(bool loading) OVERRIDE;
   virtual void SetInitialFocus(bool accept_focus) OVERRIDE;
   virtual bool InitialFocus() OVERRIDE;
+  virtual void RenderViewCreated(content::RenderViewHost *render_view_host) OVERRIDE;
+
 
   // Called to handle a mouse event.
   void HandleMouseEvent(NSEvent* event);
